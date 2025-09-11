5fdee0c6484f9d2b9aaa196300dbad2b8d557667
RatchapoomTH <ratchapoom.th@kkumail.com>
2025-09-12 00:37:54 +0700

    Delete Week 2 directory

--- Week 2/Code wk2/weight.py
+++
@@ -1,9 +0,0 @@
-# weight.py
-%%writefile converter2/weight.py
-def kilogram_to_pound(kg: float):
-    lb = kg * 2.20462
-    return lb, f"{kg:.2f} kg = {lb:.2f} lb"
-
-def pound_to_kilogram(lb: float):
-    kg = lb / 0.453592
-    return kg, f"{lb:.2f} lb = {kg:.2f} kg"
