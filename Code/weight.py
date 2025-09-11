c9509619135b08f34cf35df98c211374689f3616
RatchapoomTH <ratchapoom.th@kkumail.com>
2025-09-12 01:06:16 +0700

    Update weight.py

--- Code/weight.py
+++ Code/weight.py
@@ -1,9 +1,13 @@
 # weight.py
 %%writefile converter2/weight.py
 def kilogram_to_pound(kg: float):
-    lb = kg * 2.20462
-    return lb, f"{kg:.2f} kg = {lb:.2f} lb"
+  if kg < 0:
+    return None, "❌ Weight cannot be negative."
+  lb = kg * 2.20462
+  return lb, f"{kg:.2f} kg = {lb:.2f} lb"
 
 def pound_to_kilogram(lb: float):
-    kg = lb / 0.453592
-    return kg, f"{lb:.2f} lb = {kg:.2f} kg"
+  if lb < 0:
+    return None, "❌ Weight cannot be negative."
+  kg = lb / 0.453592
+  return kg, f"{lb:.2f} lb = {kg:.2f} kg"
