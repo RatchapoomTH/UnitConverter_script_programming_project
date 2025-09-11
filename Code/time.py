3815ace9e8814cbb3b4d1114d651c3c56820d57b
RatchapoomTH <ratchapoom.th@kkumail.com>
2025-09-12 01:07:03 +0700

    Update time.py

--- Code/time.py
+++ Code/time.py
@@ -1,17 +1,25 @@
 # time.py
 %%writefile converter2/time.py
 def hours_to_minutes(hr: float):
+    if hr < 0:
+        return None, "❌ Time cannot be negative."
     minutes = hr * 60
-    return minutes, f"{hr:.2f} hr = {minutes:.2f} min"
+    return minutes, f"{hr:.4f} hr = {minutes:.4f} min"
 
 def minutes_to_hours(minutes: float):
+    if minutes < 0:
+        return None, "❌ Time cannot be negative."
     hr = minutes / 60
-    return hr, f"{minutes:.2f} min = {hr:.2f} hr"
+    return hr, f"{minutes:.4f} min = {hr:.4f} hr"
 
 def minutes_to_seconds(minutes: float):
+    if minutes < 0:
+        return None, "❌ Time cannot be negative."
     sec = minutes * 60
-    return sec, f"{minutes:.2f} min = {sec:.2f} sec"
+    return sec, f"{minutes:.4f} min = {sec:.4f} sec"
 
 def seconds_to_minutes(sec: float):
+    if sec < 0:
+        return None, "❌ Time cannot be negative."
     minutes = sec / 60
-    return minutes, f"{sec:.2f} sec = {minutes:.2f} min"
+    return minutes, f"{sec:.4f} sec = {minutes:.4f} min"
